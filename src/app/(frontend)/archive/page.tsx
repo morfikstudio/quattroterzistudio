8b34817119d71473b20400ca8d8c6488ccaf26d2
Fausto Renier <fausto.renier@gmail.com>
2026-04-18 11:58:14 +0200

    feat: :sparkles: added isSelected flag to Sanity Studio projects

--- src/app/(frontend)/archive/page.tsx
+++ src/app/(frontend)/archive/page.tsx
@@ -1,10 +1,10 @@
 import { sanityFetch } from "@/sanity/lib/client"
-import { PROJECTS_QUERY } from "@/sanity/lib/queries"
+import { ARCHIVE_PROJECTS_QUERY } from "@/sanity/lib/queries"
 import ProjectsList from "@/components/ProjectsList"
 import ProjectsListPlain from "@/components/ProjectsListPlain"
 
 export default async function Page() {
-  const projects = await sanityFetch({ query: PROJECTS_QUERY })
+  const projects = await sanityFetch({ query: ARCHIVE_PROJECTS_QUERY })
   return (
     <main className="h-screen overflow-hidden">
       <ProjectsListPlain projects={projects} />
